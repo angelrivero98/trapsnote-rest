19d385edd9e02d477d628cfff08206cd37851a73
Angel Rivero <angelantonio3000@gmail.com>
2017-12-01 17:02:51 -0400

    arreglo con el post

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ app.post('/tareas', (req, res) => {
 //Guarda un nuevo usuario en db mandada por servidor
 app.post('/usuarios', (req, res) => {
   var usuario = new Usuario({
+    username:req.body.username,
     name: req.body.name,
     last_name: req.body.last_name,
     email: req.body.email,
