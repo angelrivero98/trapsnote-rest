0322b35a6b2fda1f772d8cc65c8ba1f28e193ba8
Angel Rivero <angelantonio3000@gmail.com>
2017-12-03 12:34:26 -0400

    arreglos con el patch

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var md5 = require('md5');
 const _ = require('lodash');
 
@@ -14,6 +15,35 @@ const port = process.env.PORT || 3000;
 
 //Manda un json a restAPI
 app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  var usuario = new Usuario({
+    email: req.body.email,
+    password: req.body.password
+  });
+  var password = usuario.password;
+  var email = usuario.email;
+  Usuario.findOne({ email: email }).then((usuario) => {
+    if (!usuario) {
+      res.status(500).send();
+    }
+    if (md5(password) != usuario.password) {
+      console.log('Contraseña incorrecta');
+      if (usuario.intentos < 5) {
+        usuario.intentos++;
+        usuario.save();
+      } else {
+        usuario.bloqueado = true;
+        usuario.save();
+      }
+      res.status(400).send();
+    } else {
+      usuario.intentos = 0;
+      usuario.save();
+      res.send({usuario});
+    }
+  }).catch((e) => res.status(400).send());
+});
 
 //Guarda una nueva tarea en db mandada por servidor
 app.post('/tareas', (req, res) => {
@@ -31,13 +61,13 @@ app.post('/tareas', (req, res) => {
 //Guarda un nuevo usuario en db mandada por servidor
 app.post('/usuarios', (req, res) => {
   var usuario = new Usuario({
-    username:req.body.username,
+    username: req.body.username,
     name: req.body.name,
     last_name: req.body.last_name,
     email: req.body.email,
-
-    password: md5(req.body.password)
-
+    password: md5(req.body.password),
+    intentos: 0,
+    bloqueado: false
   });
 
   usuario.save().then((doc) => {
@@ -49,7 +79,7 @@ app.post('/usuarios', (req, res) => {
 
 
 
-//Obtiene un usuario del servidor
+//Obtiene los usuarios del servidor
 
 app.get('/usuarios',(req,res)=>{
   Usuario.find().then((usuarios) =>{
@@ -68,22 +98,6 @@ app.get('/tareas', (req,res) => {
   });
 });
 
-
-//Obtiene una usuario según su id
-app.get('/usuarios/:id', (req, res) => {
-  var id = req.params.id; // el id lo pasamos como parametro para despues validarlo
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send(); // Si el ID no es valido devuelve una respuesta 404
-  }
-  Usuario.findById(id).then((usuario) => { //Se realiza la busqueda del usuario por ID
-    if (!usuario) {
-      return res.status(404).send(); // Si el usuario no existe devuelve una respuesta 404
-    }
-    res.send({usuario}); // Si todo estuvo bien, devuelve el usuario
-  }).catch((e) => res.status(400).send()); // Si hubo un error lo atrapa y devuelve una respuesta 400
-});
-
-//Obtiene un usuario por el username
 app.get('/usuarios/:username', (req, res) => {
   var username = req.params.username;
   Usuario.findOne({
@@ -118,6 +132,21 @@ app.patch('/usuarios/:username',(req,res) =>{
     res.send({usuario}); // Si todo estuvo bien, devuelve el usuario
   }).catch((e) => res.status(400).send());
 });
+
+//Obtiene una usuario según su id
+app.get('/usuarios/:id', (req, res) => {
+  var id = req.params.id; // el id lo pasamos como parametro para despues validarlo
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send(); // Si el ID no es valido devuelve una respuesta 404
+  }
+  Usuario.findById(id).then((usuario) => { //Se realiza la busqueda del usuario por ID
+    if (!usuario) {
+      return res.status(404).send(); // Si el usuario no existe devuelve una respuesta 404
+    }
+    res.send({usuario}); // Si todo estuvo bien, devuelve el usuario
+  }).catch((e) => res.status(400).send()); // Si hubo un error lo atrapa y devuelve una respuesta 400
+});
+
 
 //Obtiene una tarea según su id
 
